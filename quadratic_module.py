98fbbe1649acc71ced50f3e206a09e0a94bc6383
Josiah Clark <59117744+clarkjosiah9@users.noreply.github.com>
2025-07-20 11:40:57 -0400

    Update quadratic_module.py

--- quadratic_module.py
+++ quadratic_module.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def estimate_coef(x,y):
-    return np.polyfit(x,y,2) #uses residual squares method to find coefficients in the quadratic
+    return np.polyfit(x,y,2) #uses residual squares method to find coefficients in the quadratic (see https://numpy.org/doc/stable/reference/generated/numpy.polyfit.html)
         
 def plot_regression_quad(x,y,a,b,c):
     y_pred = a*(x**2) + b*x + c
@@ -18,12 +18,12 @@ class fit: #class where each instance of x and y values contains its predicted q
         
         self.a,self.b,self.c = estimate_coef(self.x,self.y)
     def __str__(self):
-        return f'y = {self.a}x^2 + {self.b}x + {self.c}'  
+        return f'y = {self.a}x^2 + {self.b}x + {self.c}' #gives equation when printed
     def predict(self,x_0):
-        y_pred = self.a*(x_0**2) + self.b*(x_0) + self.c
+        y_pred = self.a*(x_0**2) + self.b*(x_0) + self.c #returns predicted value 
         return y_pred
     def graph(self, x_axis = '', y_axis = ''):
-        plot_regression_quad(self.x,self.y,self.a,self.b,self.c)
+        plot_regression_quad(self.x,self.y,self.a,self.b,self.c) #plots and shows the graph of the quadratic
         plt.xlabel(f'{x_axis}')
         plt.ylabel(f'{y_axis}')
         plt.show()
